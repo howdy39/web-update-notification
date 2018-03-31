f34e37ba21c14fc33ab28d3424af99b253182001
howdy39 <tatsuya.howdy@gmail.com>
2018-04-01 00:50:59 +0900

    変数を一箇所にまとめる

--- gce/src/observe-google-news.js
+++ gce/src/observe-google-news.js
@@ -3,21 +3,25 @@
 const puppeteer = require('puppeteer');
 const notificationService = require('./shared/notification.service');
 
+const url = 'https://news.google.com/news/?ned=jp&gl=JP&hl=ja';
+const title = 'GoogleNews';
+const selector = '.nuEeue.hzdq5d.ME7ew';
+
 (async () => {
-  const url = 'https://news.google.com/news/?ned=jp&gl=JP&hl=ja';
+
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
-  const result = await page.evaluate(() => {
+  const result = await page.evaluate((selector) => {
     return {
-      content: document.querySelector('.nuEeue.hzdq5d.ME7ew').innerText
-    };
-  });
+      content: document.querySelector(selector).innerText
+    }
+  }, selector);
 
   console.log('result:', result);
 
   await browser.close();
 
-  notificationService.postMessage(url, 'GoogleNews', result.content);
+  notificationService.postMessage(url, title, result.content);
 })();
